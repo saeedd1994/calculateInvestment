f5503956ede2b29b7c5e92b5f7aeac9cbec0712f
Saeed Dashti <saeed.da1994@gmail.com>
2025-10-28 15:51:44 +0330

    using currency pipe in component

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {GridResultModelInterface} from "../models/grid-result-model.interface";
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe
+  ],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
